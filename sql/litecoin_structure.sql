7fe381f44c640277fd48a25516e0bc8994360b87
Sebastian Grewe <sebastian@grewe.ca>
2013-05-10 22:43:30 +0200

    adding new table strcuture, removed unused tables

--- sql/litecoin_structure.sql
+++ sql/litecoin_structure.sql
@@ -1,28 +1,32 @@
--- MySQL dump 10.13  Distrib 5.5.31, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: litecoin
--- ------------------------------------------------------
--- Server version	5.5.31-log
+-- Host: localhost
+-- Erstellungszeit: 10. Mai 2013 um 22:41
+-- Server Version: 5.5.31-0ubuntu0.13.04.1
+-- PHP-Version: 5.4.9-4ubuntu2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `accountBalance`
+-- Datenbank: `mmcfe_ng`
 --
 
-DROP TABLE IF EXISTS `accountBalance`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `accountBalance` (
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `accountBalance`
+--
+
+CREATE TABLE IF NOT EXISTS `accountBalance` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `userId` int(255) NOT NULL,
   `balance` varchar(40) DEFAULT NULL,
@@ -32,17 +36,53 @@ CREATE TABLE `accountBalance` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `userId` (`userId`),
   KEY `b_userId` (`userId`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `ledger`
+-- Tabellenstruktur für Tabelle `accounts`
 --
 
-DROP TABLE IF EXISTS `ledger`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ledger` (
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `admin` int(1) NOT NULL,
+  `username` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `pass` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL COMMENT 'Assocaited email: used for validating users, and re-setting passwords',
+  `loggedIp` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `sessionTimeoutStamp` int(255) NOT NULL,
+  `pin` varchar(255) CHARACTER SET latin1 NOT NULL COMMENT 'four digit pin to allow account changes',
+  `donate_percent` varchar(11) CHARACTER SET latin1 DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `blocks`
+--
+
+CREATE TABLE IF NOT EXISTS `blocks` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `height` int(10) unsigned NOT NULL,
+  `blockhash` char(64) CHARACTER SET utf8 NOT NULL,
+  `confirmations` int(10) unsigned NOT NULL,
+  `amount` float NOT NULL,
+  `time` int(11) NOT NULL,
+  `accounted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `height` (`height`,`blockhash`),
+  KEY `timestamp` (`time`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Discovered blocks persisted from Litecoin Service' AUTO_INCREMENT=25 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `ledger`
+--
+
+CREATE TABLE IF NOT EXISTS `ledger` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `userId` int(255) NOT NULL,
   `transType` varchar(40) DEFAULT NULL,
@@ -52,35 +92,15 @@ CREATE TABLE `ledger` (
   `assocBlock` int(255) DEFAULT '0',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `networkBlocks`
+-- Tabellenstruktur für Tabelle `pool_worker`
 --
 
-DROP TABLE IF EXISTS `networkBlocks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `networkBlocks` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `blockNumber` int(255) NOT NULL,
-  `timestamp` int(255) NOT NULL,
-  `accountAddress` varchar(255) NOT NULL,
-  `confirms` int(255) NOT NULL,
-  `difficulty` varchar(240) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14158 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `pool_worker`
---
-
-DROP TABLE IF EXISTS `pool_worker`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pool_worker` (
+CREATE TABLE IF NOT EXISTS `pool_worker` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `associatedUserId` int(255) NOT NULL,
   `username` char(50) DEFAULT NULL,
@@ -89,31 +109,27 @@ CREATE TABLE `pool_worker` (
   `hashrate` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `p_username` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `settings`
+-- Tabellenstruktur für Tabelle `settings`
 --
 
-DROP TABLE IF EXISTS `settings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `settings` (
+CREATE TABLE IF NOT EXISTS `settings` (
   `setting` varchar(255) NOT NULL,
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`setting`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `shares`
+-- Tabellenstruktur für Tabelle `shares`
 --
 
-DROP TABLE IF EXISTS `shares`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shares` (
+CREATE TABLE IF NOT EXISTS `shares` (
   `id` bigint(30) NOT NULL AUTO_INCREMENT,
   `rem_host` varchar(255) NOT NULL,
   `username` varchar(120) NOT NULL,
@@ -123,147 +139,8 @@ CREATE TABLE `shares` (
   `solution` varchar(257) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1506135 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1507278 ;
 
---
--- Table structure for table `shares_counted`
---
-
-DROP TABLE IF EXISTS `shares_counted`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shares_counted` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `blockNumber` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `invalid` int(11) NOT NULL DEFAULT '0',
-  `counted` int(1) NOT NULL DEFAULT '1',
-  `score` double(23,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27107 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `shares_history`
---
-
-DROP TABLE IF EXISTS `shares_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shares_history` (
-  `id` bigint(30) NOT NULL AUTO_INCREMENT,
-  `counted` int(1) NOT NULL COMMENT 'BOOLEAN) Tells server if it used these shares for counting',
-  `blockNumber` int(255) NOT NULL,
-  `rem_host` varchar(255) NOT NULL,
-  `username` varchar(120) NOT NULL,
-  `our_result` enum('Y','N') NOT NULL,
-  `upstream_result` enum('Y','N') DEFAULT NULL,
-  `reason` varchar(50) DEFAULT NULL,
-  `solution` varchar(257) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `score` double(23,2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `sh_blocknumber` (`blockNumber`),
-  KEY `sh_counted` (`counted`)
-) ENGINE=InnoDB AUTO_INCREMENT=1519630 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `shares_uncounted`
---
-
-DROP TABLE IF EXISTS `shares_uncounted`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shares_uncounted` (
-  `id` bigint(30) NOT NULL AUTO_INCREMENT,
-  `blockNumber` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `invalid` int(11) NOT NULL DEFAULT '0',
-  `counted` int(1) NOT NULL DEFAULT '0',
-  `score` double(23,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27856 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `userHashrates`
---
-
-DROP TABLE IF EXISTS `userHashrates`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userHashrates` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `userId` int(255) NOT NULL,
-  `hashrate` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `timestamp` (`timestamp`),
-  KEY `userHashrates_id1` (`userId`),
-  KEY `userId_timestamp` (`userId`,`timestamp`)
-) ENGINE=InnoDB AUTO_INCREMENT=133959 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `webUsers`
---
-
-DROP TABLE IF EXISTS `webUsers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `webUsers` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `admin` int(1) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL COMMENT 'Assocaited email: used for validating users, and re-setting passwords',
-  `emailAuthPin` varchar(10) NOT NULL COMMENT 'The pin required to authorize that email address',
-  `secret` varchar(10) NOT NULL,
-  `loggedIp` varchar(255) NOT NULL,
-  `sessionTimeoutStamp` int(255) NOT NULL,
-  `accountLocked` int(255) NOT NULL COMMENT 'This is the timestamp when the account will be unlocked(usually used to lock accounts that are trying to be bruteforced)',
-  `accountFailedAttempts` int(2) NOT NULL COMMENT 'This counts the number of failed attempts for web login',
-  `pin` varchar(255) NOT NULL COMMENT 'four digit pin to allow account changes',
-  `share_count` int(11) DEFAULT NULL,
-  `stale_share_count` int(11) DEFAULT NULL,
-  `shares_this_round` int(11) DEFAULT NULL,
-  `api_key` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
-  `activeEmail` int(1) DEFAULT NULL,
-  `hashrate` int(11) DEFAULT NULL,
-  `donate_percent` varchar(11) DEFAULT '0',
-  `round_estimate` varchar(40) DEFAULT '0',
-  `account_type` int(1) NOT NULL DEFAULT '0' COMMENT '0 = normal account, 9 = early-adopter no-fee',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `winning_shares`
---
-
-DROP TABLE IF EXISTS `winning_shares`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `winning_shares` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `blockNumber` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `shareCount` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-05-06 14:05:24
