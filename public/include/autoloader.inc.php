38d2d5abd72428d9c37e28ec4cca3397731dffac
Sebastian Grewe <sebastian@grewe.ca>
2013-05-13 23:29:19 +0200

    bringing class loader in order

--- public/include/autoloader.inc.php
+++ public/include/autoloader.inc.php
@@ -7,8 +7,12 @@ require_once(INCLUDE_DIR . '/smarty.inc.php');
 // Load classes that need the above as dependencies
 require_once(CLASS_DIR . '/block.class.php');
 require_once(CLASS_DIR . '/user.class.php');
-require_once(CLASS_DIR . '/worker.class.php');
 require_once(CLASS_DIR . '/share.class.php');
+require_once(CLASS_DIR . '/worker.class.php');
 require_once(CLASS_DIR . '/statistics.class.php');
 require_once(CLASS_DIR . '/transaction.class.php');
 require_once(CLASS_DIR . '/settings.class.php');
+
+// Use Memcache to store our data
+$memcache = new Memcached();
+$memcache->addServer('localhost', 11211);
