128e09cd5db316c5e97e4c137f6d85fa51fb1f0d
Sebastian Grewe <sebastian@grewe.ca>
2013-05-23 11:10:17 +0200

    initial work on password reset, not working fully yet

---
+++ public/include/pages/password/reset.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// Make sure we are called from index.php
+if (!defined('SECURITY'))
+    die('Hacking attempt');
+
+if ($user->resetPassword($_POST['username'])) {
+  $_SESSION['POPUP'][] = array('CONTENT' => 'Please check your mail account to finish your password reset');
+} else {
+  $_SESSION['POPUP'][] = array('CONTENT' => $user->getError(), 'TYPE' => 'errormsg');
+}
+
+// Tempalte specifics, user default template by parent page
+$smarty->assign("CONTENT", "../default.tpl");
+?>
