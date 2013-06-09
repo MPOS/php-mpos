af9d2aed9534fc535546c42a4c9a6c614343e737
Sebastian Grewe <sebastian@grewe.ca>
2013-06-09 15:31:38 +0200

    wrong syntax in upgrade SQL

--- sql/issue_144_notification_upgrade.sql
+++ sql/issue_144_notification_upgrade.sql
@@ -1,4 +1,4 @@
-ALTER TABLE  `notifications` ADD  `account_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (  `account_id` )
+ALTER TABLE  `notifications` ADD  `account_id` INT UNSIGNED NULL DEFAULT NULL , ADD INDEX (  `account_id` );
 CREATE TABLE IF NOT EXISTS `notification_settings` (
   `type` varchar(15) NOT NULL,
   `account_id` int(11) NOT NULL,
