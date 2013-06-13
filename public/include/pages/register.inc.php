a703877122e8c89e2888973be66afcc193b40629
Sebastian Grewe <sebastian@grewe.ca>
2013-06-13 13:38:32 +0200

    Adding support do disable account registration
    * Adding new configuration variable, see `global.inc.dist.php`
    * If you are not able to register anymore check the config var is
    * set
    
    Requested in and fixes #150

--- public/include/pages/register.inc.php
+++ public/include/pages/register.inc.php
@@ -1,9 +1,13 @@
 <?php
 
 // Make sure we are called from index.php
-if (!defined('SECURITY'))
-    die('Hacking attempt');
+if (!defined('SECURITY')) die('Hacking attempt');
 
-// Tempalte specifics
-$smarty->assign("CONTENT", "default.tpl");
+if (!$config['website']['registration']) {
+  $_SESSION['POPUP'][] = array('CONTENT' => 'Account registration is currently disabled. Please try again later.', 'TYPE' => 'errormsg');
+  $smarty->assign("CONTENT", "disabled.tpl");
+} else {
+  // Tempalte specifics
+  $smarty->assign("CONTENT", "default.tpl");
+}
 ?>
