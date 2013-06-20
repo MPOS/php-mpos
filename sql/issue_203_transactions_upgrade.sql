5eaf4cb3505ef22ebfec6dcfb8ff768a2016eadf
Sebastian Grewe <sebastian@grewe.ca>
2013-06-20 16:45:14 +0200

    Adding Bonus to issue-203 upgrade SQL

--- sql/issue_203_transactions_upgrade.sql
+++ sql/issue_203_transactions_upgrade.sql
@@ -1,1 +1,1 @@
-ALTER TABLE  `transactions` CHANGE  `type`  `type` ENUM(  'Credit',  'Debit_MP',  'Debit_AP',  'Donation',  'Fee',  'Orphan_Credit',  'Orphan_Fee',  'Orphan_Donation',  'Credit_PPS',  'Fee_PPS',  'Donation_PPS',  'TXFee' ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
+ALTER TABLE  `transactions` CHANGE  `type`  `type` ENUM(  'Credit',  'Debit_MP',  'Debit_AP',  'Donation',  'Fee',  'Orphan_Credit',  'Orphan_Fee',  'Orphan_Donation',  'Credit_PPS',  'Fee_PPS',  'Donation_PPS',  'TXFee', 'Bonus') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
