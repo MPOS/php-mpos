84ababe9f72e16b2bb9616b6f1b1f8090f3fc956
Sebastian Grewe <sebastian@grewe.ca>
2013-06-21 12:11:13 +0200

    Adding support for post time and author
    * Added new SQL upgrade and structure
    * Added post time and author to admin panel
    * Added post time and author to news list
    
    Fixes #226

--- public/include/classes/news.class.php
+++ public/include/classes/news.class.php
@@ -23,7 +23,7 @@ class News extends Base {
    **/
   public function getAllActive() {
     $this->debug->append("STA " . __METHOD__, 4);
-    $stmt = $this->mysqli->prepare("SELECT * FROM $this->table WHERE active = 1");
+    $stmt = $this->mysqli->prepare("SELECT n.*, a.username AS author FROM $this->table AS n LEFT JOIN " . $this->user->getTableName() . " AS a ON a.id = n.account_id WHERE active = 1");
     if ($stmt && $stmt->execute() && $result = $stmt->get_result())
       return $result->fetch_all(MYSQLI_ASSOC);
     // Catchall
@@ -35,7 +35,7 @@ class News extends Base {
    **/
   public function getAll() {
     $this->debug->append("STA " . __METHOD__, 4);
-    $stmt = $this->mysqli->prepare("SELECT * FROM $this->table");
+    $stmt = $this->mysqli->prepare("SELECT n.*, a.username AS author FROM $this->table AS n LEFT JOIN " . $this->user->getTableName() . " AS a ON a.id = n.account_id");
     if ($stmt && $stmt->execute() && $result = $stmt->get_result())
       return $result->fetch_all(MYSQLI_ASSOC);
     // Catchall
@@ -98,5 +98,5 @@ class News extends Base {
 $news = new News();
 $news->setDebug($debug);
 $news->setMysql($mysqli);
-
+$news->setUser($user);
 ?>
--- public/templates/mmcFE/news/default.tpl
+++ public/templates/mmcFE/news/default.tpl
@@ -1,5 +1,5 @@
 {section name=news loop=$NEWS}
-  {include file="global/block_header.tpl" BLOCK_HEADER="{$NEWS[news].header}"}
+  {include file="global/block_header.tpl" BLOCK_HEADER="{$NEWS[news].header}  posted {$NEWS[news].time} by {$NEWS[news].author}"}
   {$NEWS[news].content}
   {include file="global/block_footer.tpl"}
 {/section}
