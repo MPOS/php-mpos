4eaa2c8d37c583e66d33d53b04ff3ad9f3fc01b0
Sebastian Grewe <sebastian@grewe.ca>
2013-06-21 20:21:27 +0200

    hotfix for news page accessible by all users

--- public/include/pages/admin/news.inc.php
+++ public/include/pages/admin/news.inc.php
@@ -3,6 +3,12 @@
 // Make sure we are called from index.php
 if (!defined('SECURITY')) die('Hacking attempt');
 
+// Check user to ensure they are admin
+if (!$user->isAuthenticated() || !$user->isAdmin($_SESSION['USERDATA']['id'])) {
+  header("HTTP/1.1 404 Page not found");
+  die("404 Page not found");
+}
+
 // Include markdown library
 use \Michelf\Markdown;
 
