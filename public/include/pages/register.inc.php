3e07263ae9023ba39c6644b411860984dfee4d9b
Sebastian Grewe <sebastian@grewe.ca>
2013-06-30 12:29:52 +0200

    Removed registration configuration option
    * Removed old registration configuration option from config file
    * Changed database registration option to allow registrations by default
    * Changed admin settings template for new DB option
    * Changed registration controllers for new DB option
    
    Fixes an issue with registrations not working if settings table is
    empty.

--- public/include/pages/register.inc.php
+++ public/include/pages/register.inc.php
@@ -3,7 +3,7 @@
 // Make sure we are called from index.php
 if (!defined('SECURITY')) die('Hacking attempt');
 
-if (!$config['website']['registration'] || !$setting->getValue('registration')) {
+if ($setting->getValue('lock_registration')) {
   $_SESSION['POPUP'][] = array('CONTENT' => 'Account registration is currently disabled. Please try again later.', 'TYPE' => 'errormsg');
   $smarty->assign("CONTENT", "disabled.tpl");
 } else {
