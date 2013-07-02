691611eb13bdd0ee166c000baadbc583637dcf75
obigal <abuco1@msn.com>
2013-07-02 14:43:11 -0400

    Update share.class.php

--- public/include/classes/share.class.php
+++ public/include/classes/share.class.php
@@ -277,6 +277,7 @@ class Share {
       SUBSTRING_INDEX( `username` , '.', 1 ) AS account, id
       FROM $this->table
       WHERE upstream_result = 'Y'
+      AND UNIX_TIMESTAMP(time) BETWEEN (?) AND (? + 120)
       AND id > ?
       AND UNIX_TIMESTAMP(time) >= ?
       AND UNIX_TIMESTAMP(time) <= ( ? + 60 )
