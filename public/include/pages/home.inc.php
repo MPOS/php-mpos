bffeea07c93ced2830ed4cc1ed0e89b5de947352
Sebastian Grewe <sebastian@grewe.ca>
2013-07-06 18:36:11 +0200

    Adding cache detection to many pages
    This will allow pages to skip loading data from backends like the
    database or the wallet RPC server. If a cached page is detected and
    valid, all dynamic content generation will be skipped completely.
    
    Other pages that have not been adjusted in this commit will still fetch
    backend data all the time. This will ensure clients always see the most
    recent data, like worker information or account changes.
    
    This should fix #309 completely but needs some testing.

--- public/include/pages/home.inc.php
+++ public/include/pages/home.inc.php
@@ -6,13 +6,18 @@ if (!defined('SECURITY')) die('Hacking attempt');
 // Include markdown library
 use \Michelf\Markdown;
 
-// Fetch active news to display
-$aNews = $news->getAllActive();
-if (is_array($aNews)) {
-  foreach ($aNews as $key => $aData) {
-    // Transform Markdown content to HTML
-    $aNews[$key]['content'] = Markdown::defaultTransform($aData['content']);
+if (!$smarty->isCached('master.tpl', md5(serialize($_REQUEST)))) {
+  $debug->append('No cached version available, fetching from backend', 3);
+  // Fetch active news to display
+  $aNews = $news->getAllActive();
+  if (is_array($aNews)) {
+    foreach ($aNews as $key => $aData) {
+      // Transform Markdown content to HTML
+      $aNews[$key]['content'] = Markdown::defaultTransform($aData['content']);
+    }
   }
+} else {
+  $debug->append('Using cached page', 3);
 }
 
 // Load news entries for Desktop site and unauthenticated users
--- public/include/smarty_globals.inc.php
+++ public/include/smarty_globals.inc.php
@@ -2,7 +2,7 @@
 
 // Make sure we are called from index.php
 if (!defined('SECURITY'))
-    die('Hacking attempt');
+  die('Hacking attempt');
 
 // Globally available variables
 $debug->append('Global smarty variables', 3);
@@ -109,7 +109,7 @@ if (@$_SESSION['USERDATA']['id']) {
 
   // Site-wide notifications, based on user events
   if ($aGlobal['userdata']['balance']['confirmed'] >= $config['ap_threshold']['max'])
-    $_SESSION['POPUP'][] = array('CONTENT' => 'You have exceeded the pools configured ' . $config['currency'] . ' warning threshold. Please initiate a transfer!', 'TYPE' => 'warning');
+    $_SESSION['POPUP'][] = array('CONTENT' => 'You have exceeded your accounts balance. Please transfer some ' . $config['currency'] . "!", 'TYPE' => 'errormsg');
   if ($user->getUserFailed($_SESSION['USERDATA']['id']) > 0)
     $_SESSION['POPUP'][] = array('CONTENT' => 'You have ' . $user->getUserFailed($_SESSION['USERDATA']['id']) . ' failed login attempts! <a href="?page=account&action=reset_failed">Reset Counter</a>', 'TYPE' => 'errormsg');
 }
