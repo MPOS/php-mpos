0f00f7d322826ecfc2f64e77f377f9031cec1275
Sebastian Grewe <sebastian@grewe.ca>
2013-07-15 13:46:20 +0200

    Another fix for registration issues
    Addresses #330

--- public/include/pages/register.inc.php
+++ public/include/pages/register.inc.php
@@ -6,7 +6,7 @@ if (!defined('SECURITY')) die('Hacking attempt');
 if ($setting->getValue('lock_registration') && $setting->getValue('disable_invitations')) {
   $_SESSION['POPUP'][] = array('CONTENT' => 'Account registration is currently disabled. Please try again later.', 'TYPE' => 'errormsg');
   $smarty->assign("CONTENT", "disabled.tpl");
-} else if (!$setting->getValue('disable_invitations') && !isset($_GET['token'])) {
+} else if ($setting->getValue('lock_registration') && !$setting->getValue('disable_invitations') && !isset($_GET['token'])) {
   $_SESSION['POPUP'][] = array('CONTENT' => 'Only invited users are allowed to register.', 'TYPE' => 'errormsg');
   $smarty->assign("CONTENT", "disabled.tpl");
 } else {
