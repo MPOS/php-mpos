8f9b3a79999e064f54798311a28c166f6710a4ba
nrpatten <nrpatten@gmail.com>
2013-09-07 00:12:41 +1000

    Update contactform.inc.php
    Opps

--- public/include/pages/contactform.inc.php
+++ public/include/pages/contactform.inc.php
@@ -6,10 +6,12 @@ if (!defined('SECURITY')) die('Hacking attempt');
 if ($setting->getValue('recaptcha_enabled')) {
   require_once(INCLUDE_DIR . '/lib/recaptchalib.php');
   $smarty->assign("RECAPTCHA", recaptcha_get_html($setting->getValue('recaptcha_public_key')));
-
-  // Tempalte specifics
- $smarty->assign("CONTENT", "default.tpl");
 }
+
+// Tempalte specifics
+$smarty->assign("CONTENT", "default.tpl");
+
 ?>
+
 
 
