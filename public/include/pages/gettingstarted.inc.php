19734294222210869b1519e5bad812048504dc20
root <rafta@gmx.net>
2013-10-31 13:09:30 +0100

    [IMPROVED] forgot declaration in sites

--- public/include/pages/gettingstarted.inc.php
+++ public/include/pages/gettingstarted.inc.php
@@ -4,6 +4,10 @@
 if (!defined('SECURITY'))
     die('Hacking attempt');
 
+$smarty->assign("SITESTRATUMPORT", $config['gettingstarted']['coinname']);
+$smarty->assign("SITECOINNAME", $config['gettingstarted']['coinname']);
+$smarty->assign("SITECOINURL", $config['gettingstarted']['coinurl']);
+
 // Tempalte specifics
 $smarty->assign("CONTENT", "default.tpl");
 ?>
