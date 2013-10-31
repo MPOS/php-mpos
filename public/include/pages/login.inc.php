fc4b7180677abb6e8efa6d46a7c91ad215d20fd1
Sebastian Grewe <sebastian@grewe.ca>
2013-10-31 07:55:12 +0100

    [FIX] Honor custom HTTP ports
    Fixes #803

--- public/include/pages/login.inc.php
+++ public/include/pages/login.inc.php
@@ -7,7 +7,7 @@ if ($setting->getValue('maintenance') && !$user->isAdmin($user->getUserId($_POST
   $_SESSION['POPUP'][] = array('CONTENT' => 'You are not allowed to login during maintenace.', 'TYPE' => 'info');
 } else if ($user->checkLogin(@$_POST['username'], @$_POST['password']) ) {
   empty($_POST['to']) ? $to = $_SERVER['PHP_SELF'] : $to = $_POST['to'];
-  $location = @$_SERVER['HTTPS'] === true ? 'https' : 'http' . '://' . $_SERVER['SERVER_NAME'] . $to;
+  $location = @$_SERVER['HTTPS'] === true ? 'https' : 'http' . '://' . $_SERVER['SERVER_NAME']  .':' . $_SERVER['SERVER_PORT'] . $to;
   if (!headers_sent()) header('Location: ' . $location);
   exit('<meta http-equiv="refresh" content="0; url=' . $location . '"/>');
 } else if (@$_POST['username'] && @$_POST['password']) {
