c048903cd0dd4ba273447870494d8270c047f736
root <rafta@gmx.net>
2013-10-31 18:10:30 +0100

    edited

--- public/include/pages/gettingstarted.inc.php
+++ public/include/pages/gettingstarted.inc.php
@@ -4,14 +4,11 @@
 if (!defined('SECURITY'))
     die('Hacking attempt');
 
-<<<<<<< HEAD
 
-
-=======
 $smarty->assign("SITESTRATUMPORT", $config['gettingstarted']['stratumport']);
 $smarty->assign("SITECOINNAME", $config['gettingstarted']['coinname']);
 $smarty->assign("SITECOINURL", $config['gettingstarted']['coinurl']);
->>>>>>> upstream/next
+
 
 // Tempalte specifics
 $smarty->assign("CONTENT", "default.tpl");
