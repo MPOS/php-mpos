ffc39f855d742deaeaf6bc2e64e0b25b01b55912
Sebastian Grewe <sebastian@grewe.ca>
2013-11-20 07:33:24 +0100

    Addressing #853 with a fix and proposed change

--- public/include/classes/transaction.class.php
+++ public/include/classes/transaction.class.php
@@ -39,9 +39,9 @@ class Transaction extends Base {
       LEFT JOIN " . $this->block->getTableName() . " AS b
       ON b.id = t.block_id
       SET t.archived = 1
-      WHERE ( t.account_id = ? AND t.id <= ? AND b.confirmations >= ? )
-      OR ( t.account_id = ? AND t.id <= ? AND t.type IN ( 'Credit_PPS', 'Donation_PPS', 'Fee_PPS', 'TXFee', 'Debit_MP', 'Debit_AP' ) )");
-    if ($this->checkStmt($stmt) && $stmt->bind_param('iiiii', $account_id, $txid, $this->config['confirmations'], $account_id, $txid) && $stmt->execute())
+      WHERE archived = 0 AND t.account_id = ? AND t.id <= ?
+      ");
+    if ($this->checkStmt($stmt) && $stmt->bind_param('ii', $account_id, $txid) && $stmt->execute())
       return true;
     return $this->sqlError();
   }
