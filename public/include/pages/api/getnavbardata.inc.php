9c101e5ca08f7d6b110bf4892aee0c3970e0d224
Sebastian Grewe <sebastian@grewe.ca>
2013-12-02 17:14:16 +0100

    [FIX] Division by 0

--- public/include/pages/api/getnavbardata.inc.php
+++ public/include/pages/api/getnavbardata.inc.php
@@ -45,7 +45,6 @@ if ($iTotalRoundShares > 0) {
 
 // Round progress
 $iEstShares = $statistics->getEstimatedShares($dDifficulty);
-$dEstPercent = round(100 / $iEstShares * $aRoundShares['valid'], 2);
 if ($iEstShares > 0 && $aRoundShares['valid'] > 0) {
     $dEstPercent = round(100 / $iEstShares * $aRoundShares['valid'], 2);
 } else {
