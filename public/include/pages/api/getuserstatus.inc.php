50634502d25c332c405887dfe13b467785efd090
Sebastian Grewe <sebastian@grewe.ca>
2013-12-08 09:45:51 +0100

    [ADDED] Transaction summary to getuserstatus API
    Fixes #917 once merged.

--- public/include/pages/api/getuserstatus.inc.php
+++ public/include/pages/api/getuserstatus.inc.php
@@ -9,12 +9,16 @@ $api->isActive();
 // Check user token
 $user_id = $api->checkAccess($user->checkApiKey($_REQUEST['api_key']), @$_REQUEST['id']);
 
+// Fetch transaction summary
+$aTransactionSummary = $transaction->getTransactionSummary($user_id);
+
 // Output JSON format
 $data = array(
   'username' => $user->getUsername($user_id),
   'shares' =>  $statistics->getUserShares($user_id),
   'hashrate' => $statistics->getUserHashrate($user_id),
-  'sharerate' => $statistics->getUserSharerate($user_id)
+  'sharerate' => $statistics->getUserSharerate($user_id),
+  'transactions' => $aTransactionSummary
 );
 echo $api->get_json($data);
 
