0d4e110f623043e64f2d9676c77f519140f60c4e
ahmedbodi <ahmed_bodi@msn.com>
2013-12-12 15:12:05 +0000

    Improved Readability

--- cronjobs/balancecron.php
+++ cronjobs/balancecron.php
@@ -2,14 +2,21 @@
 <?php
 // Change to working directory
 chdir(dirname(__FILE__));
+
 // Include all settings and classes
 require_once('shared.inc.php');
+
+// Check For RPC Connection
 if ($bitcoin->can_connect() === true){
 $dBalance = $bitcoin->query('getbalance');
+// Check Wallet Balance
 echo("The Wallet Balance is " .$dBalance. "\n");
 $dGetInfo = $bitcoin->query('getinfo');
+
+// Check for POS Mint
 if (is_array($dGetInfo) && array_key_exists('newmint', $dGetInfo)) {
 $dNewmint = $dGetInfo['newmint'];
+
 echo("Current Mint is: " .$dNewmint);
 } else {
   $dNewmint = -1;
@@ -19,18 +26,24 @@ echo("Current Mint is: " .$dNewmint);
   $dNewmint = -1;
   echo('Unable to connect to wallet RPC service');
 }
+
 // Fetch unconfirmed amount from blocks table
 empty($config['network_confirmations']) ? $confirmations = 120 : $confirmations = $config['network_confirmations'];
 $aBlocksUnconfirmed = $block->getAllUnconfirmed($confirmations);
 $dBlocksUnconfirmedBalance = 0;
 if (!empty($aBlocksUnconfirmed))
 foreach ($aBlocksUnconfirmed as $aData) $dBlocksUnconfirmedBalance += $aData['amount'];
+
 // Fetch locked balance from transactions
 $dLockedBalance = $transaction->getLockedBalance();
 echo("The Locked Wallet Balance for Users is: " .$dLockedBalance. "\n");
+// Fetch Final Wallet Balance after Transfer
 $aFloat = $dLockedBalance + $config['coldwallet']['float'];
 echo("The Locked Wallet Balance & Float amounts to: " .$aFloat. "\n");
+
+// Send Liquid Balance
 $send = $dBalance - $aFloat ;
 echo("Final Sending Amount is : " .$send. "\n");
+
 $bitcoin->query('sendtoaddress',$config['coldwallet']['address'], $send)
 ?>
