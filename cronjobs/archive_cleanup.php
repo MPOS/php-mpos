5c8a4ecb735a072fb552de13127233b9d3586225
Sebastian Grewe <sebastian@grewe.ca>
2013-12-16 16:57:27 +0100

    [FIX] Added more verbose output on failing crons for log

--- cronjobs/archive_cleanup.php
+++ cronjobs/archive_cleanup.php
@@ -27,7 +27,7 @@ require_once('shared.inc.php');
 
 // If we don't keep archives, delete some now to release disk space
 if (!$share->purgeArchive()) {
-  $log->logError("Failed to delete archived shares, not critical but should be checked!");
+  $log->logError("Failed to delete archived shares, not critical but should be checked: " . $share->getCronError());
   $monitoring->endCronjob($cron_name, 'E0008', 1, true);
 }
 
