21e83104610e8b34b9afdda3e79ddf1da69d37b7
headz <headz@motherless.com>
2013-12-16 20:50:56 -0500

    Renamed setting 'send_contactform' to 'send_contactform_to_inbox'

--- public/include/config/admin_settings.inc.php
+++ public/include/config/admin_settings.inc.php
@@ -239,7 +239,7 @@ $aSettings['system'][] = array(
   'tooltip' => 'Enable or Disable the payout processing. Users will not be able to withdraw any funds if disabled.'
 );
 $aSettings['system'][] = array(
-  'display' => 'Disable notifications', 'type' => 'select',
+  'display' => 'Disable Notifications', 'type' => 'select',
   'options' => array( 0 => 'No', 1 => 'Yes' ),
   'default' => 0,
   'name' => 'disable_notifications', 'value' => $setting->getValue('disable_notifications'),
@@ -270,7 +270,7 @@ $aSettings['system'][] = array(
   'display' => 'Send Contactform To', 'type' => 'select',
   'options' => array( 0 => 'Email', 1 => 'Inbox'),
   'default' => 1,
-  'name' => 'send_contactform', 'value' => $setting->getValue('send_contactform'),
+  'name' => 'send_contactform_to_inbox', 'value' => $setting->getValue('send_contactform_to_inbox'),
   'tooltip' => 'Where contactform messages are sent. You must enable the site inbox to receive messages there.'
 );
 $aSettings['system'][] = array(
