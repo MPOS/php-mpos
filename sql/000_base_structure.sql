04b1d826d7a0c42af9d3290d971b3f744a48abef
headz <headz@motherless.com>
2013-12-16 21:45:32 -0500

    Added icon for inbox

--- sql/000_base_structure.sql
+++ sql/000_base_structure.sql
@@ -223,6 +223,18 @@ CREATE TABLE `templates` (
   PRIMARY KEY (`template`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `inbox` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id_to` int(11) unsigned NOT NULL,
+  `account_id_from` int(11) unsigned NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_read` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `account_id_to` (`account_id_to`,`is_read`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
