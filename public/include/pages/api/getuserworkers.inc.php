e762bb0391eebc2956a242478921202a9e907c53
Sebastian Grewe <sebastian@grewe.ca>
2014-01-01 09:08:09 +0100

    [IMPROVED] Use data interval on getuserworkers

--- public/include/pages/api/getuserworkers.inc.php
+++ public/include/pages/api/getuserworkers.inc.php
@@ -9,8 +9,11 @@ $api->isActive();
 // Check user token
 $user_id = $api->checkAccess($user->checkApiKey($_REQUEST['api_key']), @$_REQUEST['id']);
 
+// Fetch data interval from admin settings
+if ( ! $interval = $setting->getValue('statistics_ajax_data_interval')) $interval = 300;
+
 // Output JSON format
-echo $api->get_json($worker->getWorkers($user_id));
+echo $api->get_json($worker->getWorkers($user_id, $interval));
 
 // Supress master template
 $supress_master = 1;
