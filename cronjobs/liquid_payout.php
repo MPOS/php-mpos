64897de040556241bfab55467d66983cde988c24
myrinx <timthurlings@gmail.com>
2014-01-10 12:18:28 +0100

    Update liquid_payout.php
    formatting

--- cronjobs/liquid_payout.php
+++ cronjobs/liquid_payout.php
@@ -8,21 +8,22 @@ require_once('shared.inc.php');
 
 // Check For RPC Connection
 if ($bitcoin->can_connect() === true){
-$dBalance = $bitcoin->getbalance();
-// Check Wallet Balance
-$log->logDebug("The Wallet Balance is " .$dBalance. "\n");
-$dGetInfo = $bitcoin->getinfo();
+	$dBalance = $bitcoin->getbalance();
+	// Check Wallet Balance
+	$log->logDebug("The Wallet Balance is " .$dBalance. "\n");
+	$dGetInfo = $bitcoin->getinfo();
 
-// Check for POS Mint
-if (is_array($dGetInfo) && array_key_exists('newmint', $dGetInfo)) {
-$dNewmint = $dGetInfo['newmint'];
-
-$log->logDebug("Current Mint is: " .$dNewmint);
-} else {
-  $dNewmint = -1;
+	// Check for POS Mint
+	
+	if (is_array($dGetInfo) && array_key_exists('newmint', $dGetInfo)) {
+		$dNewmint = $dGetInfo['newmint'];
+		$log->logDebug("Current Mint is: " .$dNewmint);
+	} else {
+		$dNewmint = -1;
+	}
 }
-} else {
-  $dBalance = 0;
+else {
+	$dBalance = 0;
   $dNewmint = -1;
   $log->logError('Unable to connect to wallet RPC service');
 }
@@ -43,15 +44,15 @@ $send = $dBalance - $dFloat ;
 $log->logInfo("Final Sending Amount is : " .$send. "\n");
 
 if($send > $dThreshold){
-        if($sendAddress !== ''){
-                $bitcoin->sendtoaddress($sendAddress, $send);
-        }
-        else {
-                $log->logInfo("No wallet address set\n");
-        }
-}
-else{
-        $log->logInfo("Final Sending Amount Not Exceed threshold : " .$send. "\n");
+  if($sendAddress !== ''){
+    $bitcoin->sendtoaddress($sendAddress, $send);
+  }
+  else {
+    $log->logInfo("No wallet address set\n");
+  }
+} 
+else {
+  $log->logInfo("Final Sending Amount Not Exceed threshold : " .$send. "\n");
 }
 ?>
 
