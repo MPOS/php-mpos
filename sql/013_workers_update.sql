08a668c962e4d176c57af67466b893c26000671f
nicoschtein <nicoschtein@gmail.com>
2014-01-16 12:20:12 -0200

    Update and rename 013_workers_update.sql to 014_workers_update.sql
    Bump DB_VERSION to 0.0.4

--- sql/013_workers_update.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `pool_worker` ADD `manual_diff` float NOT NULL DEFAULT '0' AFTER  `monitor`;
-INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '0.0.3') ON DUPLICATE KEY UPDATE `value` = '0.0.3';
-INSERT INTO `settings` (`name`, `value`) VALUES ('db_upgrade_required', 0) ON DUPLICATE KEY UPDATE `value` = 0;
