ccc7fa6fe9c6fd0ede00c63fbbaa8c7d0b3245da
xisi <chaeldarrs@gmail.com>
2014-01-19 02:02:08 -0500

    accidentally a word

--- sql/014_accounts_update.sql
+++ sql/014_accounts_update.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `accounts` ADD COLUMN `signup_timestamp` INT( 10 ) NOT NULL DEFAULT '0' AFTER `failed_pins`;
-ALTER TABLE `accounts` ADD `notify_email` VARCHAR( 255 ) NULL DEFAULT NULL AFTER `email` ;
+ALTER TABLE `accounts` ADD COLUMN `notify_email` VARCHAR( 255 ) NULL DEFAULT NULL AFTER `email` ;
 TRUNCATE TABLE `token_types`;
 INSERT INTO `token_types` (`id`, `name`, `expiration`) VALUES (1, 'password_reset', 3600), (2, 'confirm_email', 0), (3, 'invitation', 0), (4, 'account_unlock', 0), (5, 'account_edit', 360), (6, 'change_pw', 360), (7, 'withdraw_funds', 360);
 INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '0.0.4') ON DUPLICATE KEY UPDATE `value` = '0.0.4';
