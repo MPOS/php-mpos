18128fc7a716e2984a36f26cdbb11ea49d4e7522
Neozonz <neozonz@gmail.com>
2014-01-20 12:43:43 -0800

    [Optimize] Cache admin wallet via a cron to run every x minutes

---
+++ cronjobs/admin_statistics.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php
+<?php
+
+/*
+
+Copyright:: 2013, Sebastian Grewe
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ */
+
+// Change to working directory
+chdir(dirname(__FILE__));
+
+// Include all settings and classes
+require_once('shared.inc.php');
+
+$start = microtime(true);
+if (!$transaction->getLockedBalance())
+    $log->logError("getLockedBalance update failed");
+$log->logInfo("getLockedBalance " . number_format(microtime(true) - $start, 2) . " seconds");
+
+require_once('cron_end.inc.php');
+?>
