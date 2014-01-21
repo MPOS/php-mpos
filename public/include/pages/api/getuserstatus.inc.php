8f207bfe764361acc6706aadead16deb2e943ee3
Neozonz <neozonz@gmail.com>
2014-01-20 16:01:09 -0800

    [Optimize] Switched calls to username from id

--- public/include/pages/api/getuserstatus.inc.php
+++ public/include/pages/api/getuserstatus.inc.php
@@ -8,16 +8,16 @@ $api->isActive();
 
 // Check user token
 $user_id = $api->checkAccess($user->checkApiKey($_REQUEST['api_key']), @$_REQUEST['id']);
-
+$username = $user->getUsername($user_id)
 // Fetch transaction summary
 $aTransactionSummary = $transaction->getTransactionSummary($user_id);
 
 // Output JSON format
 $data = array(
-  'username' => $user->getUsername($user_id),
-  'shares' =>  $statistics->getUserShares($user_id),
-  'hashrate' => $statistics->getUserHashrate($user_id),
-  'sharerate' => $statistics->getUserSharerate($user_id)
+  'username' => $username,
+  'shares' =>  $statistics->getUserShares($username),
+  'hashrate' => $statistics->getUserHashrate($username),
+  'sharerate' => $statistics->getUserSharerate($username)
 );
 echo $api->get_json($data);
 
