d0073587105ba17abeaaffc5dff9ee43306d2a9e
Sebastian Grewe <sebastian@grewe.ca>
2014-01-29 08:52:12 +0100

    [FIX] Ignore missing HTTP_REFERRER
    Fixes #1626

--- public/include/pages/account/reset_failed.inc.php
+++ public/include/pages/account/reset_failed.inc.php
@@ -6,7 +6,7 @@ if (!defined('SECURITY')) die('Hacking attempt');
 if ($user->isAuthenticated()) {
   // Reset failed login counter
   $user->setUserFailed($_SESSION['USERDATA']['id'], 0);
-  header("Location: " . $_SERVER['HTTP_REFERER']);
+  if (!empty($_SERVER['HTTP_REFERER'])) header("Location: " . $_SERVER['HTTP_REFERER']);
 }
 // Somehow we still need to load this empty template
 $smarty->assign("CONTENT", "empty");
