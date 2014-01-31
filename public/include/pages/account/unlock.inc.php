cf8a8cc4d8c2a5d8917a0609666de78201a2d7b7
xisi <chaeldarrs@gmail.com>
2014-01-31 17:34:48 -0500

    fixes #1639

--- public/include/pages/account/unlock.inc.php
+++ public/include/pages/account/unlock.inc.php
@@ -7,15 +7,11 @@ if (!isset($_GET['token']) || empty($_GET['token'])) {
 } else if (!$aToken = $oToken->getToken($_GET['token'], 'account_unlock')) {
   $_SESSION['POPUP'][] = array('CONTENT' => 'Unable to re-activate your account. Invalid token.', 'TYPE' => 'errormsg');
 } else {
-  if (!$config['csrf']['enabled'] || $config['csrf']['enabled'] && $csrftoken->valid) {
-    if ($user->setUserFailed($aToken['account_id'], 0) && $user->setUserPinFailed($aToken['account_id'], 0) && $user->setLocked($aToken['account_id'], 0)) {
-      $oToken->deleteToken($aToken['token']);
-      $_SESSION['POPUP'][] = array('CONTENT' => 'Account re-activated. Please login.');
-    } else {
-      $_SESSION['POPUP'][] = array('CONTENT' => 'Failed to re-activate account. Contact site support.', 'TYPE' => 'errormsg');
-    }
+  if ($user->setUserFailed($aToken['account_id'], 0) && $user->setUserPinFailed($aToken['account_id'], 0) && $user->setLocked($aToken['account_id'], 0)) {
+    $oToken->deleteToken($aToken['token']);
+    $_SESSION['POPUP'][] = array('CONTENT' => 'Account re-activated. Please login.');
   } else {
-    $_SESSION['POPUP'][] = array('CONTENT' => $csrftoken->getErrorWithDescriptionHTML(), 'TYPE' => 'info');
+    $_SESSION['POPUP'][] = array('CONTENT' => 'Failed to re-activate account. Contact site support.', 'TYPE' => 'errormsg');
   }
 }
 $smarty->assign('CONTENT', 'default.tpl');
