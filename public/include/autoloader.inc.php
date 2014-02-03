d61e6a72430c4bdcf0d5dcb7cebd49fa7a7c80a8
Sebastian Grewe <sebastian@grewe.ca>
2014-02-03 07:58:06 +0100

    [FIX] Cron compile folder permission issue

--- public/include/autoloader.inc.php
+++ public/include/autoloader.inc.php
@@ -30,6 +30,10 @@ require_once(INCLUDE_DIR . '/lib/Mobile_Detect.php');
 if ($detect->isMobile() && $setting->getValue('website_mobile_theme')) {
   // Set to mobile theme
   $setting->getValue('website_mobile_theme') ? $theme = $setting->getValue('website_mobile_theme') : $theme = 'mobile';
+} else if ( PHP_SAPI == 'cli') {
+  // Create a new compile folder just for crons
+  // We call mail templates directly anyway
+  $theme = 'cron';
 } else {
   // Use configured theme, fallback to default theme
   $setting->getValue('website_theme') ? $theme = $setting->getValue('website_theme') : $theme = 'mpos';
