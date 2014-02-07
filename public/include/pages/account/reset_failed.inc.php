feeca87eb031d72aa5b1ebf28408a18614eeca7a
Sebastian Grewe <sebastian@grewe.ca>
2014-02-07 12:20:24 +0100

    [FIX] Reset failed login, redirect to trusted location
    Fixes #1701

--- public/include/pages/account/reset_failed.inc.php
+++ public/include/pages/account/reset_failed.inc.php
@@ -4,7 +4,10 @@ $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 if ($user->isAuthenticated()) {
   // Reset failed login counter
   $user->setUserFailed($_SESSION['USERDATA']['id'], 0);
-  if (!empty($_SERVER['HTTP_REFERER'])) header("Location: " . $_SERVER['HTTP_REFERER']);
+  $port = ($_SERVER["SERVER_PORT"] == "80" || $_SERVER["SERVER_PORT"] == "443") ? "" : (":".$_SERVER["SERVER_PORT"]);
+  $pushto = $_SERVER['SCRIPT_NAME'].'?page=dashboard';
+  $location = (@$_SERVER['HTTPS'] == 'on') ? 'https://' . $_SERVER['SERVER_NAME'] . $port . $pushto : 'http://' . $_SERVER['SERVER_NAME'] . $port . $pushto;
+  header("Location: " . $location);
 }
 // Somehow we still need to load this empty template
 $smarty->assign("CONTENT", "empty");
