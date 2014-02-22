b79bd6ae8e954b0ebb5bbdfbd7591f71eeb8919f
Nathan Patten <nrpatten@gmail.com>
2014-02-22 21:33:49 +1100

    [ADD] Chat

---
+++ public/include/pages/about/chat.inc.php
@@ -0,0 +1,18 @@
+<?php
+$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
+
+// ACL check
+switch($setting->getValue('acl_chat_page', 1)) {
+case '0':
+  if ($user->isAuthenticated()) {
+    $smarty->assign("CONTENT", "default.tpl");
+  }
+  break;
+case '1':
+  $smarty->assign("CONTENT", "default.tpl");
+  break;
+case '2':
+  $_SESSION['POPUP'][] = array('CONTENT' => 'Page currently disabled. Please try again later.', 'TYPE' => 'errormsg');
+  $smarty->assign("CONTENT", "disabled.tpl");
+  break;
+}
