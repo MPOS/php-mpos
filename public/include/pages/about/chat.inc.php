966d32036ba2c2efe9797065cad6aaf939a4afa7
Nathan Patten <nrpatten@gmail.com>
2014-02-24 21:12:15 +1100

    Update chat.inc.php

--- public/include/pages/about/chat.inc.php
+++ public/include/pages/about/chat.inc.php
@@ -5,10 +5,12 @@ $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 switch($setting->getValue('acl_chat_page', 2)) {
 case '0':
   if ($user->isAuthenticated()) {
+    $smarty->assign('CHATROOM', $setting->getValue('system_irc_chat', '#lazypoolop'));
     $smarty->assign("CONTENT", "default.tpl");
   }
   break;
 case '1':
+  $smarty->assign('CHATROOM', $setting->getValue('system_irc_chat', '#lazypoolop'));
   $smarty->assign("CONTENT", "default.tpl");
   break;
 case '2':
