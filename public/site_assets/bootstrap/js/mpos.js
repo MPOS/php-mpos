38a97c68119680a7e55592f510013a321b247a76
Sebastian Grewe <sebastian@grewe.ca>
2014-03-01 18:32:54 +0100

    [REMOVED] Console debug JS log

--- public/site_assets/bootstrap/js/mpos.js
+++ public/site_assets/bootstrap/js/mpos.js
@@ -8,7 +8,7 @@ $(function() {
 //collapses the sidebar on window resize.
 $(function() {
     $(window).bind("load resize", function() {
-        console.log($(this).width())
+        // console.log($(this).width())
         if ($(this).width() < 768) {
             $('div.sidebar-collapse').addClass('collapse')
         } else {
