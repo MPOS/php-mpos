0ce22ebf7adf19631968a205e4fc6a36079f09d3
Sebastian Grewe <sebastian@grewe.ca>
2014-03-03 11:44:06 +0100

    [ADDED] Template Editor

--- public/include/pages/admin/templates.inc.php
+++ public/include/pages/admin/templates.inc.php
@@ -27,7 +27,7 @@ $sOriginalTemplate = $template->getTemplateContent($sTemplate);
 
 if (@$_REQUEST['do'] == 'save') {
   if ($template->updateEntry(@$_REQUEST['template'], @$_REQUEST['content'], @$_REQUEST['active'])) {
-    $_SESSION['POPUP'][] = array('CONTENT' => 'Page updated', 'TYPE' => 'success');
+    $_SESSION['POPUP'][] = array('CONTENT' => 'Page updated', 'TYPE' => 'alert alert-success');
   } else {
     $_SESSION['POPUP'][] = array('CONTENT' => 'Page update failed: ' . $template->getError(), 'TYPE' => 'alert alert-danger');
   }
@@ -36,7 +36,7 @@ if (@$_REQUEST['do'] == 'save') {
 $oDatabaseTemplate = $template->getEntry($sTemplate);
 
 if ( $oDatabaseTemplate === false ) {
-  $_SESSION['POPUP'][] = array('CONTENT' => 'Can\'t fetch template from Database. Have you created `templates` table? Run 005_create_templates_table.sql from sql folder', 'TYPE' => 'danger');
+  $_SESSION['POPUP'][] = array('CONTENT' => 'Can\'t fetch template from Database. Have you created `templates` table? Run 005_create_templates_table.sql from sql folder', 'TYPE' => 'alert alert-danger');
 }
 
 $smarty->assign("TEMPLATES", $aTemplates);
