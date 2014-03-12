d9cfd2a9d4df42e139668b5a1d156347d2c0dc1a
iAmShorty <rafta@gmx.net>
2014-03-12 11:01:18 +0100

    [FIX] user earnings only visible as admin

--- public/include/pages/account/earnings.inc.php
+++ public/include/pages/account/earnings.inc.php
@@ -2,7 +2,7 @@
 $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
 // Check user to ensure they are admin
-if (!$user->isAuthenticated() || !$user->isAdmin($_SESSION['USERDATA']['id'])) {
+if (!$user->isAuthenticated()) {
   header("HTTP/1.1 404 Page not found");
   die("404 Page not found");
 }
