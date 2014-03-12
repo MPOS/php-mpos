fd30568af78099ced019342167d1cb3a4a66b300
Sebastian Grewe <sebastian@grewe.ca>
2014-03-12 11:03:37 +0100

    [CLEANUP] In-line with rest of account pages

--- public/include/pages/account/earnings.inc.php
+++ public/include/pages/account/earnings.inc.php
@@ -1,23 +1,20 @@
 <?php
 $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
-// Check user to ensure they are admin
+// Check user authentication status
 if (!$user->isAuthenticated()) {
-  header("HTTP/1.1 404 Page not found");
-  die("404 Page not found");
-}
-
-if (!$smarty->isCached('master.tpl', $smarty_cache_key)) {
-  $iLimit = 30;
-  $debug->append('No cached version available, fetching from backend', 3);
-  if (!$setting->getValue('disable_transactionsummary')) {
-    $aTransactionSummary = $transaction->getTransactionSummary($_SESSION['USERDATA']['id']);
-    $aTransactionSummaryByTime = $transaction->getTransactionTypebyTime($_SESSION['USERDATA']['id']);
-    $smarty->assign('SUMMARY', $aTransactionSummary);
-    $smarty->assign('BYTIME', $aTransactionSummaryByTime);
+  if (!$smarty->isCached('master.tpl', $smarty_cache_key)) {
+    $iLimit = 30;
+    $debug->append('No cached version available, fetching from backend', 3);
+    if (!$setting->getValue('disable_transactionsummary')) {
+      $aTransactionSummary = $transaction->getTransactionSummary($_SESSION['USERDATA']['id']);
+      $aTransactionSummaryByTime = $transaction->getTransactionTypebyTime($_SESSION['USERDATA']['id']);
+      $smarty->assign('SUMMARY', $aTransactionSummary);
+      $smarty->assign('BYTIME', $aTransactionSummaryByTime);
+    }
+  } else {
+    $debug->append('Using cached page', 3);
   }
-} else {
-  $debug->append('Using cached page', 3);
 }
 
 $smarty->assign('CONTENT', 'default.tpl');
