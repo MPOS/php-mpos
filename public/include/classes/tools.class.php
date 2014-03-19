daf07038573ee6b0e2338c408403f3c00be68443
iAmShorty <rafta@gmx.net>
2014-03-19 13:27:06 +0100

    [UPDATE] added mintpal ticker api

--- public/include/classes/tools.class.php
+++ public/include/classes/tools.class.php
@@ -57,6 +57,8 @@ class Tools extends Base {
       return 'cryptsy';
     } else if (preg_match('/cryptorush.in/', $url)) {
       return 'cryptorush';
+    } else if (preg_match('/mintpal.com/', $url)) {
+      return 'mintpal';
     }
     $this->setErrorMessage("API URL unknown");
     return false;
@@ -88,6 +90,9 @@ class Tools extends Base {
       	  break;
       	case 'cryptorush':
       	  return @$aData["$strCurrency/" . $this->config['price']['target']]['last_trade'];
+      	  break;
+      	case 'mintpal':
+      	  return @$aData['0']['last_price'];
       	  break;
       }
     } else {
