f467d2a414156c13ee298c6a6a87388712c42327
SamuelNZ <SamuelNZ@users.noreply.github.com>
2014-03-20 15:36:41 +1300

    Create coin_quark.class.php
    Suggested improvements welcomed

---
+++ public/include/classes/coins/coin_quark.class.php
@@ -0,0 +1,32 @@
+<?php
+$defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
+
+/**
+ * Extend Coin Base for Quark.
+ * Quark
+ * Calculated using the FAQ and the difference between stratum and mpos
+ * This will give you Stratum Difficulty of 4
+ * With a target difficulty of 18 in MPOS
+ * Not finished - Possibly not right.
+ **/
+class Coin extends CoinBase {
+  protected $target_bits = 18;
+}
+
+?>
+
+/**
+   * Calculate our hashrate based on shares inserted to DB
+   * We use diff1 share values, not a baseline one
+   **/
+  public function calcHashrate($shares, $interval) {
+    return $shares * pow(2, $this->target_bits) / $interval / 1000 / 4.5;
+  }
+
+  /**
+   * Calculate estimated shares of this coin, this is using baseline
+   * according to our configuration difficulty
+   **/
+  public function calcEstaimtedShares($dDifficulty) {
+    return (int)round((pow(2, (32 - $this->target_bits)) * $dDifficulty) / pow(2, ($this->config['difficulty'] - 16)) / 4.5);
+  }
