3541d059583f06231ffc0d205d10d3e149cbe919
iAmShorty <iAmShorty@users.noreply.github.com>
2014-03-27 17:46:11 +0100

    [FIX] socket timeout setting

--- public/include/admin_checks.php
+++ public/include/admin_checks.php
@@ -100,16 +100,16 @@ if (@$_SESSION['USERDATA']['is_admin'] && $user->isAdmin(@$_SESSION['USERDATA'][
   if (function_exists('socket_create')) {
     $host = @gethostbyname($config['gettingstarted']['stratumurl']);
     $port = $config['gettingstarted']['stratumport'];
-    socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 3, 'usec' => 0)); 
-
+    
     if (isset($port) and
       ($socket=socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) and
-      (socket_connect($socket, $host, $port)))
+      (socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 3, 'usec' => 0))) and
+      (@socket_connect($socket, $host, $port)))
     {
       socket_close($socket);
-    }
-    else
+    } else {
       $enotice[] = 'We tried to poke your Stratum server using your $config[\'gettingstarted\'] settings but it didn\'t respond - ' . socket_strerror(socket_last_error());
+    }
   } else {
     // Connect via fsockopen as fallback
     if (! $fp = @fsockopen($config['gettingstarted']['stratumurl'], $config['gettingstarted']['stratumport'], $errCode, $errStr, 1)) {
