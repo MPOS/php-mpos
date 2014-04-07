0677321259851e4b7d81d8fcfedd3512bbefba40
Sebastian Grewe <sebastian@grewe.ca>
2014-04-07 09:55:02 +0200

    [CLEANUP] Use of isset instead of @

--- public/include/classes/mail.class.php
+++ public/include/classes/mail.class.php
@@ -90,7 +90,11 @@ class Mail extends Base {
       ->setSender($senderEmail)
       ->setReturnPath($senderEmail)
       ->setBody($this->smarty->fetch(BASEPATH . 'templates/mail/' . $template . '.tpl'), 'text/html');
-    if (strlen(@$aData['senderName']) > 0 && strlen(@$aData['senderEmail']) > 0 && filter_var(@$aData['senderEmail'], FILTER_VALIDATE_EMAIL))
+    if (isset($aData['senderName']) &&
+        isset($aData['senderEmail']) &&
+        strlen($aData['senderName']) > 0 &&
+        strlen($aData['senderEmail']) > 0 &&
+        filter_var($aData['senderEmail'], FILTER_VALIDATE_EMAIL))
       $message->setReplyTo(array($aData['senderEmail'] => $aData['senderName']));
 
     // Send message out with configured transport
