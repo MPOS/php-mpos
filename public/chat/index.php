2c14eb34fae84c38baf94bf689b0cb224014f4fa
Marc Paradise <marc@getchef.com>
2014-05-27 22:36:54 -0400

    add embedded chat page

---
+++ public/chat/index.php
@@ -0,0 +1,95 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>AwesomeHash - A light-hearted mining shibe community I About</title>
+	
+	<link rel="shortcut icon" href="/site_assets/favicon.ico" />
+	<link rel="stylesheet" href="/site_assets/mpos/css/layout.css" type="text/css" media="screen" />
+  <link rel="stylesheet"   href="/site_assets/mpos/css/fontello.css">
+  <link rel="stylesheet"   href="/site_assets/mpos/css/animation.css">
+  <!--[if IE 7]><link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
+	<link rel="stylesheet" href="/site_assets/mpos/css/visualize.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="/site_assets/mpos/css/custom.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="/site_assets/mpos/css/jquery.jqplot.min.css" type="text/css" media="screen" />
+  <!--[if lt IE 9]>
+	<link rel="stylesheet" href="site_assets/mpos/css/ie.css" type="text/css" media="screen" />
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript" src="/site_assets/mpos/js/jquery-2.0.3.min.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/hideshow.js" type="text/javascript"></script>
+  <script type="text/javascript"   src="/site_assets/mpos/js/jquery.visualize.js"></script>
+  <script type="text/javascript"   src="/site_assets/mpos/js/jquery.jqplot.min.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/jquery.tablesorter.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/jquery.tablesorter.pager.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/jquery.equalHeight.js"></script>
+  <script type="text/javascript"   src="/site_assets/mpos/js/raphael.2.1.2.min.js"></script>
+  <script type="text/javascript"   src="/site_assets/mpos/js/justgage.1.0.1.min.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/custom.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/js/tinybox.js"></script>
+	<script type="text/javascript" src="/site_assets/mpos/../global/js/number_format.js"></script>
+  <!--[if IE]><script type="text/javascript" src="site_assets/mpos/js/excanvas.js"></script><![endif]-->
+  <script>
+    var zxcvbnPath = "site_assets/mpos/js/zxcvbn/zxcvbn.js";
+  </script>
+  <script type="text/javascript" src="site_assets/mpos/js/pwcheck.js"></script>
+    
+</head>
+<body>
+	<header id="header">
+         <hgroup>
+      <a href="/index.php"><h1 class="site_title"><img src='https://i.imgur.com/lcCK7m8.png' height='50'>AwesomeHash</h1></a>
+      <h2 class="section_title">Chat</h2>
+    </hgroup>
+	</header>
+	<section id="secondary_bar">
+        <div class="user">
+            <p>Welcome</p>
+    </div>
+
+        <div class="breadcrumbs_container">
+              <article class="breadcrumbs"><a href="/index.php">AwesomeHash</a> <div class="breadcrumb_divider"></div> <a class="current" >Chat</a></article>
+    </div>
+
+	</section>
+
+	<section id="main" class="column width_full" style="">
+                  <article class="module width_full" style="width:100%; height:100%">
+<header><h3>AwesomeHash</h3></header>
+<div class="module_content" style="height:100%">
+
+<iframe src="https://webchat.freenode.net/?channels=#awesomehash" height="100%" width="100%" style="height:100%"></iframe>
+
+</div>
+</article>
+          		<div class="spacer" style='clear:both;'></div>
+	</section>
+	<div class="spacer" style='clear:both;'></div>
+  <footer class="footer" style='clear:both;'>
+        <p><strong>MPOS</strong> by TheSerapher, available on <a href="https://github.com/MPOS/php-mpos">GitHub</a></p>
+    <p>Please <strong>Donate</strong> to support contests and giveaways here on AwesomeHash: DOGE DTSSpy6S1Xut6VH2mJ9VH5TDQqmHfDhUPY</p>
+    <p>Please <strong>Donate</strong> to TheSerapher LTC: Lge95QR2frp9y1wJufjUPCycVsg5gLJPW8</p>
+    <p><strong>Copyright &copy; 2013 Sebastian Grewe</strong>, Theme by <a href="http://www.medialoot.com">MediaLoot</a></p>
+    
+      <!-- Piwik -->
+        <script type="text/javascript">
+          var _paq = _paq || [];
+          _paq.push(["trackPageView"]);
+          _paq.push(["enableLinkTracking"]);
+        
+          (function() {
+            var u=(("https:" == document.location.protocol) ? "https" : "http") + "://awesomehash.com/stats/";
+            _paq.push(["setTrackerUrl", u+"piwik.php"]);
+            _paq.push(["setSiteId", "2"]);
+            var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+            g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Piwik Code -->
+    
+    
+  </footer>
+</body>
+</html>
