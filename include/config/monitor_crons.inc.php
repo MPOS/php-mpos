a8ba98a3b0cb9e3d4f11d29f0cd8321fe0f0eeed
David Meyer <manebit@zbs.com>
2014-07-15 21:41:18 -0700

    Removed redundant switch statement. Fixed closing tag.

--- include/config/monitor_crons.inc.php
+++ include/config/monitor_crons.inc.php
@@ -1,17 +1,6 @@
 <?php
-
 // Small helper array that may be used on some page controllers to
 // fetch the crons we wish to monitor
-switch ($config['payout_system']) {
-  case 'pplns':
-    $sPayoutSystem = $config['payout_system'] . '_payout';
-    break;
-  case 'pps':
-    $sPayoutSystem = $config['payout_system'] . '_payout';
-    break;
-  case 'prop':
-    $sPayoutSystem = 'proportional_payout';
-    break;
-}
-
+$sPayoutSystem = $config['payout_system'] . '_payout';
 $aMonitorCrons = array('statistics','tickerupdate','notifications','tables_cleanup','findblock',$sPayoutSystem,'blockupdate','payouts');
+?>
