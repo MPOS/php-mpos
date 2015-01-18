4bf899bb84d37d243c1ec31c003628ad2b7ef6a9
Sebastian Grewe <sebastian.grewe@xing.com>
2015-01-18 15:42:25 +0100

    [FIX] Proper version in base import sql

--- sql/000_base_structure.sql
+++ sql/000_base_structure.sql
@@ -144,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
   UNIQUE KEY `setting` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '0.0.15');
+INSERT INTO `settings` (`name`, `value`) VALUES ('DB_VERSION', '1.0.0');
 
 CREATE TABLE IF NOT EXISTS `shares` (
   `id` bigint(30) NOT NULL AUTO_INCREMENT,
