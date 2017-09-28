6c1aa1587918f413dc6315ecc6815068fc32d9b4
shtse8 <shtse8@gmail.com>
2017-09-28 11:44:33 +0000

    Correct SERVER_NAME to HTTP_HOST to get a valid host url

--- include/lib/swiftmailer/classes/Swift/Mime/SimpleMimeEntity.php
+++ include/lib/swiftmailer/classes/Swift/Mime/SimpleMimeEntity.php
@@ -687,7 +687,7 @@ class Swift_Mime_SimpleMimeEntity implements Swift_Mime_MimeEntity
     protected function getRandomId()
     {
         $idLeft = md5(getmypid() . '.' . time() . '.' . uniqid(mt_rand(), true));
-        $idRight = !empty($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'swift.generated';
+        $idRight = !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'swift.generated';
         $id = $idLeft . '@' . $idRight;
 
         try {
