fa872e74048f3ddbf2e8dd0ced62b38b00e63bd4
Jason Egan <jason.egan@gmail.com>
2018-05-02 15:54:31 -0400

    $statistics, not $this

--- include/pages/api/getactiveusers.php
+++ include/pages/api/getactiveusers.php
@@ -8,8 +8,8 @@ $api->isActive();
 $user_id = $api->checkAccess($user->checkApiKey($_REQUEST['api_key']), @$_REQUEST['id']);
 
 // Output JSON format
-if ($this->getGetCache() && $data = $this->memcache->getStatic(STATISTICS_ALL_USER_HASHRATES)) {
-    echo $api->get_json($this->memcache->getStatic(STATISTICS_ALL_USER_HASHRATES));
+if ($this->getGetCache() && $data = $statistics->memcache->getStatic(STATISTICS_ALL_USER_HASHRATES)) {
+    echo $api->get_json($statistics->memcache->getStatic(STATISTICS_ALL_USER_HASHRATES));
 } else {
     echo $api->get_json('');
 }
