085dfa05f2a3f3840f3cbe1b4fca7e6149bd59e5
Sebastian Grewe <sebastian.grewe@gmail.com>
2018-05-23 23:03:23 +0200

    [VERSION] 1.1.0

--- include/version.inc.php
+++ include/version.inc.php
@@ -1,7 +1,7 @@
 <?php
 $defflip = (!cfip()) ? exit(header('HTTP/1.1 401 Unauthorized')) : 1;
 
-define('MPOS_VERSION', '1.0.9');
+define('MPOS_VERSION', '1.1.0');
 define('DB_VERSION', '1.0.3');
 define('CONFIG_VERSION', '1.0.1');
 define('HASH_VERSION', 1);
